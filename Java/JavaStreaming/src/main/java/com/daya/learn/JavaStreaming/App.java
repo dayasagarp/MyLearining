fbbb5f6faa502c217f54d19ae4992aa5a3bda548
Dayasagar Patil <dayasagarpatil@gmail.com>
2022-12-24 13:16:39 +0530

    Java Streaming First Check in

---
+++ Java/JavaStreaming/src/main/java/com/daya/learn/JavaStreaming/App.java
@@ -0,0 +1,13 @@
+package com.daya.learn.JavaStreaming;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
